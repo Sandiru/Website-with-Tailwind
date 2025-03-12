74a6430304d7adbce581032d532075658b0b84eb
Sandiru <ssandiru@gmail.com>
2025-03-12 23:46:26 +0530

    Added design elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.35rem] overflow-hidden">
+        <Header />
+        <Hero />
+      </div>
+      <ButtonGradient />
+    </>
+  );
 }
 
 export default App;
